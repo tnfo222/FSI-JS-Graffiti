9b03e2d3e660720b4afdc392afbacc7966a70349
tnfo222 <github.com/tnfo222/psychic-disco>
2022-01-27 19:14:29 -0500

    changed header alignment, changed border and color style to the footer, all images have a border radius of 50px and have been rotated 180 deg, and the dog's names have been aligned to the left.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+let dogImages = document.querySelectorAll('.dog-image')
+let dogNames = document.querySelectorAll('.dog-name')
+let footer = document.querySelector('.footer')
+footer.style.color = 'blue'
+footer.style.borderStyle = 'solid'
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'rotate(180deg)'
+}
+for (let j = 0; j < dogNames.length; j++) {
+    dogNames[j].style.textAlign = 'left'
+}
 // Select some elements...
